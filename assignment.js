4b6f9f2ffe762f0c6834a8a6b2f76f696ec1ec59
Nikhil Sahu <heynikhil07@gmail.com>
2023-04-07 18:09:07 +0530

    Done the assignment of Equality operators-loose&strict

--- assignment.js
+++ assignment.js
@@ -53,7 +53,19 @@ if(population > avgPopulation){
 
 //Type Conversion and Coercion
 
-console.log('9'-'5');
-console.log('19'-'13'+'17');
-console.log('123' < 57);
-console.log(5+6+'4'+9 -4-2);
+console.log('9'-'5'); //4
+console.log('19'-'13'+'17'); //614
+console.log('123' < 57); //false
+console.log(5+6+'4'+9 -4-2); //1143
+
+//Equality Operators: == vs. ===
+
+const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+if (numNeighbours === 1){ //loose equality operator not recommded
+    console.log("Only 1 border");
+} else if (numNeighbours > 1){
+    console.log("More than 1 border");
+} else{
+    console.log("No borders");
+}
