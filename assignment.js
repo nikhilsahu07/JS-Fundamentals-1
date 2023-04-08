59ff70ad48c8b53fa65d131e1486ea2dbd001d30
Nikhil Sahu <heynikhil07@gmail.com>
2023-04-08 10:06:17 +0530

    Done assignment of switch statements

--- assignment.js
+++ assignment.js
@@ -78,3 +78,27 @@ if (language === 'English' && population < 50 && !isIsland){
     console.log(`${country} does not meet your criteria.`);
 }
 
+//the Switch statements
+
+switch (language){
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers..!');
+        break;
+    case 'spanish':
+    case 'espanol':
+        console.log('2nd place in number of native speakers..');
+        break;
+    case 'english':
+        console.log('3rd place.');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken.');
+        break;
+    default:
+        console.log('Great language too :D');
+        break;
+}
